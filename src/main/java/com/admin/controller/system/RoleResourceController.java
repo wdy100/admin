fd462b13dde18786d9adf1620fb1af971d05c49d
王帅帅 <shuaishuai.wang@rrs.com>
2017-11-20 02:04:44 +0800

    权限管理

--- src/main/java/com/admin/controller/system/RoleResourceController.java
+++ src/main/java/com/admin/controller/system/RoleResourceController.java
@@ -1,16 +1,29 @@
 package com.admin.controller.system;
 
+import com.admin.entity.system.Role;
 import com.admin.entity.system.User;
+import com.admin.service.system.RoleResourceService;
+import com.admin.service.system.RoleService;
 import com.admin.service.system.UserService;
+import com.haier.common.BusinessException;
 import com.haier.common.ServiceResult;
+import com.haier.common.util.JsonUtil;
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,21 +38,60 @@ public class RoleResourceController {
     @Resource  
     private UserService userService;
 
+    @Resource
+    private RoleService roleService;
+    
+    @Resource
+    private RoleResourceService roleResourceService;
+    
     @RequestMapping(value = "roleResource.html", method = { RequestMethod.GET, RequestMethod.POST })
-    public String index(HttpServletRequest request, Model model) throws Exception {
-        String mobile = "18765996558";
-        ServiceResult<User> result = this.userService.getByMobile(mobile);
-        model.addAttribute("user", result.getResult());
+    public String index(@RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer description,
+            @RequestParam(required = false) Integer rows,
+            @RequestParam(required = false) Integer page,
+            HttpServletRequest request, HttpServletResponse response,
+            Map<String, Object> modelMap) throws Exception {
+
         return "system/roleResource_list";
     }
     
     @RequestMapping("/roleResourceList")
-    public String roleResourceList(HttpServletRequest request,Map<String, Object> stack){
-        String mobile = request.getParameter("mobile");  
-        ServiceResult<User> result = this.userService.getByMobile(mobile); 
-        log.info("测试2");
-        //model.addAttribute("user", result.getResult());
-        stack.put("user", result.getResult());
-        return "showUser2";  
+    public void roleResourceList(@RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer description,
+            @RequestParam(required = false) Integer rows,
+            @RequestParam(required = false) Integer page,
+            HttpServletRequest request, HttpServletResponse response,
+            Map<String, Object> modelMap) throws Exception {
+    	 try {
+             if (rows == null)
+                 rows = 20;
+             if (page == null)
+                 page = 1;
+             Map<String, Object> params = new HashMap<String, Object>();
+             params.put("m", (page - 1) * rows);
+             params.put("n", rows);
+             //参数加入params里
+             params.put("name", name);
+             params.put("description", description);
+
+             ServiceResult<List<Role>> result = roleService.getRoleList(params);
+             if(result == null || !result.getSuccess()) {
+                 log.error("权限列表查询失败");
+                 throw new BusinessException("权限列表查询失败");
+             }
+             List<Role> roleList = result.getResult();
+
+             //获得条数
+             int resultcount = roleList.size();
+             Map<String, Object> retMap = new HashMap<String, Object>();
+             retMap.put("total", resultcount);
+             retMap.put("rows", roleList);
+             response.getWriter().write(JsonUtil.toJson(retMap));
+             response.getWriter().flush();
+             response.getWriter().close();
+         } catch (IOException e) {
+             log.error("权限列表查询失败", e);
+             throw new BusinessException("权限列表查询失败" + e.getMessage());
+         }
     }  
 }  
--- src/main/java/com/admin/service/impl/system/RoleResourceServiceImpl.java
+++ src/main/java/com/admin/service/impl/system/RoleResourceServiceImpl.java
@@ -1,15 +1,14 @@
 package com.admin.service.impl.system;
 
-import com.admin.dao.system.UserDao;
-import com.admin.entity.system.User;
-import com.admin.service.system.RoleResourceService;
-import com.admin.service.system.RoleService;
-import com.haier.common.ServiceResult;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import com.admin.dao.system.RoleResourceDao;
+import com.admin.entity.system.User;
+import com.admin.service.system.RoleResourceService;
+import com.haier.common.ServiceResult;
 
 /**
  * Created by GaoJingFei on 2017/11/13.
@@ -18,14 +17,10 @@ import static com.google.common.base.Preconditions.checkNotNull;
 @Service
 public class RoleResourceServiceImpl implements RoleResourceService {
     @Autowired
-    private UserDao userDao;
+    private RoleResourceDao roleResourceDao;
     @Override
     public ServiceResult<User> getByMobile(String mobile){
-        checkNotNull(mobile, "mobile不能为空");
         ServiceResult<User> result = new ServiceResult<User>();
-        User user = userDao.getByMobile(mobile);
-        log.info("根据手机号查询用户信息,mobile={}", mobile);
-        result.setResult(user);
         return result;
     }
 }
