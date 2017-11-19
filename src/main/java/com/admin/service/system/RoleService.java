375b02fb3c207c786b4e35373a43de969ccd0f8f
admin <admin>
2017-11-19 23:42:27 +0800

    角色，页面、服务修改

--- src/main/java/com/admin/service/system/RoleService.java
+++ src/main/java/com/admin/service/system/RoleService.java
@@ -1,6 +1,10 @@
 package com.admin.service.system;
+import com.admin.entity.system.Role;
 import com.admin.entity.system.User;
 import com.haier.common.ServiceResult;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by GaoJingFei on 2017/11/13.
@@ -11,4 +15,8 @@ public interface RoleService {
      * 根据手机号获取用户信息
      * */
     public ServiceResult<User> getByMobile(String mobile);
+
+    public ServiceResult<List<Role>> getRoleList(Map<String, Object> paramMap);
+
+    public ServiceResult<Integer> insert(Role role);
 }
