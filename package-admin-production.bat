bc07e3fb73ec1ca5a65c92724de91509ffd7b922
GaoJingFei <Jingfei.Gao@rrs.com>
2017-11-22 10:14:30 +0800

    打包命令修改

--- package-admin-production.bat
+++ package-admin-production.bat
@@ -1,4 +1,4 @@
-call mvn clean package -Dmaven.test.skip=true -Pproduction
+call mvn clean package -Dmaven.test.skip=true -Pprod
 copy .\target\ROOT.war
 @echo on
 echo 'Build project successfully!'
