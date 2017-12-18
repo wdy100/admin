ca5e85131de08eaafa8578891b5889a08f6368f5
liuxian <liuxian@rrs.com>
2017-12-18 18:19:33 +0800

    报表管理  人员费用支出

---
+++ src/main/java/com/admin/dao/reports/ExpenseReportsDao.java
@@ -0,0 +1,11 @@
+package com.admin.dao.reports;
+
+import com.admin.dao.BaseDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by liuxian on 2017/12/18.
+ */
+@Repository
+public class ExpenseReportsDao extends BaseDao {
+}
