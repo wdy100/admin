07dbefd1994b8bcd79d08a885e2a846513923480
liuxian <liuxian@rrs.com>
2017-12-19 10:17:39 +0800

    报表管理  人员费用支出  提交

---
+++ src/main/java/com/admin/entity/reports/ExpenseReports.java
@@ -0,0 +1,40 @@
+package com.admin.entity.reports;
+
+import com.admin.entity.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by lx on 2017/12/19.
+ */
+@ToString(callSuper=true)
+@Getter
+@Setter
+public class ExpenseReports extends BaseEntity {
+    private static final long serialVersionUID = 2648004986369887978L;
+
+    private String name;
+
+    private Integer month;
+
+    private BigDecimal fuelCharge;
+
+    private BigDecimal busFee;
+
+    private BigDecimal taxiFee;
+
+    private BigDecimal telephoneCharge;
+
+    private BigDecimal feteFee;
+
+    private BigDecimal travellingExpenses;
+
+    private BigDecimal marketingCosts;
+
+    private BigDecimal allCosts;
+
+    private Integer enable;
+}
