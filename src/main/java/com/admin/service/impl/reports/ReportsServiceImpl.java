98bd14df3134de1dd986648082250e023de56a07
admin <admin>
2017-12-24 13:36:44 +0800

    业绩完成率报表 人员累计和月度累计 提交

--- src/main/java/com/admin/service/impl/reports/ReportsServiceImpl.java
+++ src/main/java/com/admin/service/impl/reports/ReportsServiceImpl.java
@@ -1,10 +1,14 @@
 package com.admin.service.impl.reports;
 
 import com.admin.dao.reports.ExpenseReportsDao;
+import com.admin.dao.reports.PerformanceReportsDao;
 import com.admin.entity.reports.ExpenseReports;
-import com.admin.service.reports.ExpenseReportsService;
+import com.admin.entity.reports.PerformanceReports;
+import com.admin.service.reports.ReportsService;
 import com.haier.common.ServiceResult;
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +20,11 @@ import java.util.Map;
  */
 @Slf4j
 @Service
-public class ExpenseReportsServiceImpl implements ExpenseReportsService {
+public class ReportsServiceImpl implements ReportsService {
     @Autowired
     private ExpenseReportsDao expenseReportsDao;
+    @Autowired
+    private PerformanceReportsDao performanceReportsDao;
 
     @Override
     public ServiceResult<List<ExpenseReports>> getExpenseReportsListByPerson(Map<String, Object> paramMap) {
@@ -32,6 +38,22 @@ public class ExpenseReportsServiceImpl implements ExpenseReportsService {
     public ServiceResult<List<ExpenseReports>> getExpenseReportsListByMonth(Map<String, Object> paramMap) {
         ServiceResult<List<ExpenseReports>> result = new ServiceResult<List<ExpenseReports>>();
         result.setResult(expenseReportsDao.getExpenseReportsListByMonth(paramMap));
+
+        return result;
+    }
+
+    @Override
+    public ServiceResult<List<PerformanceReports>> getPerformanceReportsListByPerson(Map<String, Object> paramMap) {
+        ServiceResult<List<PerformanceReports>> result = new ServiceResult<List<PerformanceReports>>();
+        result.setResult(performanceReportsDao.getPerformanceReportsListByPerson(paramMap));
+
+        return result;
+    }
+
+    @Override
+    public ServiceResult<List<PerformanceReports>> getPerformanceReportsListByMonth(Map<String, Object> paramMap) {
+        ServiceResult<List<PerformanceReports>> result = new ServiceResult<List<PerformanceReports>>();
+        result.setResult(performanceReportsDao.getPerformanceReportsListByMonth(paramMap));
 
         return result;
     }
--- src/main/java/com/admin/service/reports/ReportsService.java
+++ src/main/java/com/admin/service/reports/ReportsService.java
@@ -1,6 +1,7 @@
 package com.admin.service.reports;
 
 import com.admin.entity.reports.ExpenseReports;
+import com.admin.entity.reports.PerformanceReports;
 import com.haier.common.ServiceResult;
 
 import java.util.List;
@@ -9,9 +10,13 @@ import java.util.Map;
 /**
  * Created by lx on 2017/12/19.
  */
-public interface ExpenseReportsService {
+public interface ReportsService {
 
     public ServiceResult<List<ExpenseReports>> getExpenseReportsListByPerson(Map<String, Object> paramMap);
 
     public ServiceResult<List<ExpenseReports>> getExpenseReportsListByMonth(Map<String, Object> paramMap);
+
+    public ServiceResult<List<PerformanceReports>> getPerformanceReportsListByPerson(Map<String, Object> paramMap);
+
+    public ServiceResult<List<PerformanceReports>> getPerformanceReportsListByMonth(Map<String, Object> paramMap);
 }
